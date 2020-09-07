beb3be47da90bda859b11b80904847246d7838b0
junior624 <antoniojunior1987@gmail.com>
2020-09-07 16:08:01 -0300

    js

--- JS/script.js
+++ JS/script.js
@@ -7,3 +7,18 @@ function perguntar () {
     nome = prompt("Qual seu nome? ");
     alert("Olá, "+ nome);
 }
+
+function mudar_texto(){
+    var h1 = document.getElementsByTagName("h1");
+
+    if(h1[0].innerText == "Geek University"){
+        h1[0].innerText = "Evolua seu lado geek!"
+    }else{
+        h1[0].innerText = "Geek University"
+    }
+}
+
+function incrementar() {
+    var p1 = document.getElementById("p1");
+    p1.innerText = parseInt(p1.innerText) + 1;
+}
