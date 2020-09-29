47c35dda7805969cde2f23efb4563a4825d65062
junior624 <antoniojunior1987@gmail.com>
2020-09-29 20:55:49 -0300

    aulas

---
+++ Secao05/aula03foreach.js
@@ -0,0 +1,22 @@
+var cursos = [
+    'Programação para leigos',
+    'Algoritimos e lógica de programação',
+    'Programação em C',
+    'Programação em Java',
+    'Programação em Python',
+    'Banco de dados',
+    'Programação Web com Django Framework',
+    'Programação em JavaScript',
+];
+//console.log(cursos);
+//console.log(cursos.length);
+
+// definir função
+
+function imprimir(curso, indice){
+    console.log(indice + ' - ' + curso);
+}
+
+//cursos.forEach(imprimir);
+
+//utilização de uma função anonima (lambda/callback) template string
